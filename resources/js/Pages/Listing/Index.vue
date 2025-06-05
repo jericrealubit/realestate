c3ddb3972430e93a2b128dea746679bd56120817
jeric realubit <jericrealubit@gmail.com>
2025-06-06 09:43:19 +1200

    composables

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,9 +1,5 @@
 <script setup>
-import { Link } from "@inertiajs/vue3";
-import Box from "@/Components/UI/Box.vue";
-import ListingAddress from "@/Components/ListingAddress.vue";
-import ListingSpace from "@/Components/ListingSpace.vue";
-import Price from "@/Components/Price.vue";
+import Listing from "@/Pages/Listing/Index/Compoments/Listing.vue";
 
 defineProps({
     listings: Array,
@@ -12,24 +8,10 @@ defineProps({
 
 <template>
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-        <Box v-for="listing in listings" :key="listing.id">
-            <div>
-                <Link :href="route('listing.show', listing.id)">
-                    <Price :price="listing.price" class="text-2xl" />
-                    <ListingSpace :listing="listing" class="text-lg" />
-                    <ListingAddress :listing="listing" class="text-gray-500" />
-                </Link>
-            </div>
-            <div>
-                <Link :href="route('listing.edit', listing.id)">Edit</Link>
-            </div>
-            <div>
-                <Link
-                    :href="route('listing.destroy', listing.id)"
-                    method="DELETE"
-                    >Delete</Link
-                >
-            </div>
-        </Box>
+        <Listing
+            v-for="listing in listings"
+            :key="listing.id"
+            :listing="listing"
+        />
     </div>
 </template>
